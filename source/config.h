5a9a46fc14514518fd4d0c84320e89ccb8a09586
alexlazar <afla@itu.dk>
2017-02-27 15:50:38 +0100

    upd

--- source/config.h
+++ source/config.h
@@ -1,5 +1,6 @@
 #undef __INTEL_COMPILER
 #undef __clang__
 #undef CONFIG_X86_32
+#undef CONFIG_OPTIMIZE_INLINING
 
 #define __printf(X, Y)
--- source/include/linux/bug.h
+++ source/include/linux/bug.h
@@ -2,109 +2,109 @@
 #define _LINUX_BUG_H
 
 #include <asm/bug.h>
-// /*AFLA*/ #include <linux/compiler.h>
-// /*AFLA*/ 
-// /*AFLA*/ enum bug_trap_type {
-// /*AFLA*/ 	BUG_TRAP_TYPE_NONE = 0,
-// /*AFLA*/ 	BUG_TRAP_TYPE_WARN = 1,
-// /*AFLA*/ 	BUG_TRAP_TYPE_BUG = 2,
-// /*AFLA*/ };
-// /*AFLA*/ 
-// /*AFLA*/ struct pt_regs;
-// /*AFLA*/ 
-// /*AFLA*/ #ifdef __CHECKER__
-// /*AFLA*/ #define __BUILD_BUG_ON_NOT_POWER_OF_2(n) (0)
-// /*AFLA*/ #define BUILD_BUG_ON_NOT_POWER_OF_2(n) (0)
-// /*AFLA*/ #define BUILD_BUG_ON_ZERO(e) (0)
-// /*AFLA*/ #define BUILD_BUG_ON_NULL(e) ((void*)0)
-// /*AFLA*/ #define BUILD_BUG_ON_INVALID(e) (0)
-// /*AFLA*/ #define BUILD_BUG_ON_MSG(cond, msg) (0)
-// /*AFLA*/ #define BUILD_BUG_ON(condition) (0)
-// /*AFLA*/ #define BUILD_BUG() (0)
-// /*AFLA*/ #define MAYBE_BUILD_BUG_ON(cond) (0)
-// /*AFLA*/ #else /* __CHECKER__ */
-// /*AFLA*/ 
-// /*AFLA*/ /* Force a compilation error if a constant expression is not a power of 2 */
-// /*AFLA*/ #define __BUILD_BUG_ON_NOT_POWER_OF_2(n)	\
-// /*AFLA*/ 	BUILD_BUG_ON(((n) & ((n) - 1)) != 0)
-// /*AFLA*/ #define BUILD_BUG_ON_NOT_POWER_OF_2(n)			\
-// /*AFLA*/ 	BUILD_BUG_ON((n) == 0 || (((n) & ((n) - 1)) != 0))
-// /*AFLA*/ 
-// /*AFLA*/ /* Force a compilation error if condition is true, but also produce a
-// /*AFLA*/    result (of value 0 and type size_t), so the expression can be used
-// /*AFLA*/    e.g. in a structure initializer (or where-ever else comma expressions
-// /*AFLA*/    aren't permitted). */
-// /*AFLA*/ #define BUILD_BUG_ON_ZERO(e) (sizeof(struct { int:-!!(e); }))
-// /*AFLA*/ #define BUILD_BUG_ON_NULL(e) ((void *)sizeof(struct { int:-!!(e); }))
-// /*AFLA*/ 
-// /*AFLA*/ /*
-// /*AFLA*/  * BUILD_BUG_ON_INVALID() permits the compiler to check the validity of the
-// /*AFLA*/  * expression but avoids the generation of any code, even if that expression
-// /*AFLA*/  * has side-effects.
-// /*AFLA*/  */
-// /*AFLA*/ #define BUILD_BUG_ON_INVALID(e) ((void)(sizeof((__force long)(e))))
-// /*AFLA*/ 
-// /*AFLA*/ /**
-// /*AFLA*/  * BUILD_BUG_ON_MSG - break compile if a condition is true & emit supplied
-// /*AFLA*/  *		      error message.
-// /*AFLA*/  * @condition: the condition which the compiler should know is false.
-// /*AFLA*/  *
-// /*AFLA*/  * See BUILD_BUG_ON for description.
-// /*AFLA*/  */
-// /*AFLA*/ #define BUILD_BUG_ON_MSG(cond, msg) compiletime_assert(!(cond), msg)
-// /*AFLA*/ 
-// /*AFLA*/ /**
-// /*AFLA*/  * BUILD_BUG_ON - break compile if a condition is true.
-// /*AFLA*/  * @condition: the condition which the compiler should know is false.
-// /*AFLA*/  *
-// /*AFLA*/  * If you have some code which relies on certain constants being equal, or
-// /*AFLA*/  * some other compile-time-evaluated condition, you should use BUILD_BUG_ON to
-// /*AFLA*/  * detect if someone changes it.
-// /*AFLA*/  *
-// /*AFLA*/  * The implementation uses gcc's reluctance to create a negative array, but gcc
-// /*AFLA*/  * (as of 4.4) only emits that error for obvious cases (e.g. not arguments to
-// /*AFLA*/  * inline functions).  Luckily, in 4.3 they added the "error" function
-// /*AFLA*/  * attribute just for this type of case.  Thus, we use a negative sized array
-// /*AFLA*/  * (should always create an error on gcc versions older than 4.4) and then call
-// /*AFLA*/  * an undefined function with the error attribute (should always create an
-// /*AFLA*/  * error on gcc 4.3 and later).  If for some reason, neither creates a
-// /*AFLA*/  * compile-time error, we'll still have a link-time error, which is harder to
-// /*AFLA*/  * track down.
-// /*AFLA*/  */
-// /*AFLA*/ #ifndef __OPTIMIZE__
-// /*AFLA*/ #define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
-// /*AFLA*/ #else
-// /*AFLA*/ #define BUILD_BUG_ON(condition) \
-// /*AFLA*/ 	BUILD_BUG_ON_MSG(condition, "BUILD_BUG_ON failed: " #condition)
-// /*AFLA*/ #endif
-// /*AFLA*/ 
-// /*AFLA*/ /**
-// /*AFLA*/  * BUILD_BUG - break compile if used.
-// /*AFLA*/  *
-// /*AFLA*/  * If you have some code that you expect the compiler to eliminate at
-// /*AFLA*/  * build time, you should use BUILD_BUG to detect if it is
-// /*AFLA*/  * unexpectedly used.
-// /*AFLA*/  */
-// /*AFLA*/ #define BUILD_BUG() BUILD_BUG_ON_MSG(1, "BUILD_BUG failed")
-// /*AFLA*/ 
-// /*AFLA*/ #define MAYBE_BUILD_BUG_ON(cond)			\
-// /*AFLA*/ 	do {						\
-// /*AFLA*/ 		if (__builtin_constant_p((cond)))       \
-// /*AFLA*/ 			BUILD_BUG_ON(cond);             \
-// /*AFLA*/ 		else                                    \
-// /*AFLA*/ 			BUG_ON(cond);                   \
-// /*AFLA*/ 	} while (0)
-// /*AFLA*/ 
-// /*AFLA*/ #endif	/* __CHECKER__ */
-// /*AFLA*/ 
-// /*AFLA*/ #ifdef CONFIG_GENERIC_BUG
-// /*AFLA*/ #include <asm-generic/bug.h>
-// /*AFLA*/ 
-// /*AFLA*/ static inline int is_warning_bug(const struct bug_entry *bug)
-// /*AFLA*/ {
-// /*AFLA*/ 	return bug->flags & BUGFLAG_WARNING;
-// /*AFLA*/ }
-// /*AFLA*/ 
+#include <linux/compiler.h>
+
+enum bug_trap_type {
+	BUG_TRAP_TYPE_NONE = 0,
+	BUG_TRAP_TYPE_WARN = 1,
+	BUG_TRAP_TYPE_BUG = 2,
+};
+
+struct pt_regs;
+
+#ifdef __CHECKER__
+#define __BUILD_BUG_ON_NOT_POWER_OF_2(n) (0)
+#define BUILD_BUG_ON_NOT_POWER_OF_2(n) (0)
+#define BUILD_BUG_ON_ZERO(e) (0)
+#define BUILD_BUG_ON_NULL(e) ((void*)0)
+#define BUILD_BUG_ON_INVALID(e) (0)
+#define BUILD_BUG_ON_MSG(cond, msg) (0)
+#define BUILD_BUG_ON(condition) (0)
+#define BUILD_BUG() (0)
+#define MAYBE_BUILD_BUG_ON(cond) (0)
+#else /* __CHECKER__ */
+
+/* Force a compilation error if a constant expression is not a power of 2 */
+#define __BUILD_BUG_ON_NOT_POWER_OF_2(n)	\
+	BUILD_BUG_ON(((n) & ((n) - 1)) != 0)
+#define BUILD_BUG_ON_NOT_POWER_OF_2(n)			\
+	BUILD_BUG_ON((n) == 0 || (((n) & ((n) - 1)) != 0))
+
+/* Force a compilation error if condition is true, but also produce a
+   result (of value 0 and type size_t), so the expression can be used
+   e.g. in a structure initializer (or where-ever else comma expressions
+   aren't permitted). */
+#define BUILD_BUG_ON_ZERO(e) (sizeof(struct { int:-!!(e); }))
+#define BUILD_BUG_ON_NULL(e) ((void *)sizeof(struct { int:-!!(e); }))
+
+/*
+ * BUILD_BUG_ON_INVALID() permits the compiler to check the validity of the
+ * expression but avoids the generation of any code, even if that expression
+ * has side-effects.
+ */
+#define BUILD_BUG_ON_INVALID(e) ((void)(sizeof((__force long)(e))))
+
+/**
+ * BUILD_BUG_ON_MSG - break compile if a condition is true & emit supplied
+ *		      error message.
+ * @condition: the condition which the compiler should know is false.
+ *
+ * See BUILD_BUG_ON for description.
+ */
+#define BUILD_BUG_ON_MSG(cond, msg) compiletime_assert(!(cond), msg)
+
+/**
+ * BUILD_BUG_ON - break compile if a condition is true.
+ * @condition: the condition which the compiler should know is false.
+ *
+ * If you have some code which relies on certain constants being equal, or
+ * some other compile-time-evaluated condition, you should use BUILD_BUG_ON to
+ * detect if someone changes it.
+ *
+ * The implementation uses gcc's reluctance to create a negative array, but gcc
+ * (as of 4.4) only emits that error for obvious cases (e.g. not arguments to
+ * inline functions).  Luckily, in 4.3 they added the "error" function
+ * attribute just for this type of case.  Thus, we use a negative sized array
+ * (should always create an error on gcc versions older than 4.4) and then call
+ * an undefined function with the error attribute (should always create an
+ * error on gcc 4.3 and later).  If for some reason, neither creates a
+ * compile-time error, we'll still have a link-time error, which is harder to
+ * track down.
+ */
+#ifndef __OPTIMIZE__
+#define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
+#else
+#define BUILD_BUG_ON(condition) \
+	BUILD_BUG_ON_MSG(condition, "BUILD_BUG_ON failed: " #condition)
+#endif
+
+/**
+ * BUILD_BUG - break compile if used.
+ *
+ * If you have some code that you expect the compiler to eliminate at
+ * build time, you should use BUILD_BUG to detect if it is
+ * unexpectedly used.
+ */
+#define BUILD_BUG() BUILD_BUG_ON_MSG(1, "BUILD_BUG failed")
+
+#define MAYBE_BUILD_BUG_ON(cond)			\
+	do {						\
+		if (__builtin_constant_p((cond)))       \
+			BUILD_BUG_ON(cond);             \
+		else                                    \
+			BUG_ON(cond);                   \
+	} while (0)
+
+#endif	/* __CHECKER__ */
+
+#ifdef CONFIG_GENERIC_BUG
+#include <asm-generic/bug.h>
+
+static inline int is_warning_bug(const struct bug_entry *bug)
+{
+	return bug->flags & BUGFLAG_WARNING;
+}
+
 // /*AFLA*/ const struct bug_entry *find_bug(unsigned long bugaddr);
 // /*AFLA*/ 
 // /*AFLA*/ enum bug_trap_type report_bug(unsigned long bug_addr, struct pt_regs *regs);
@@ -120,5 +120,5 @@
 // /*AFLA*/ 	return BUG_TRAP_TYPE_BUG;
 // /*AFLA*/ }
 // /*AFLA*/ 
-// /*AFLA*/ #endif	/* CONFIG_GENERIC_BUG */
+#endif	/* CONFIG_GENERIC_BUG */
 #endif	/* _LINUX_BUG_H */
